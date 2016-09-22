4bfefed6b60fdf01feef107a3debe14487579384
gg271828 <1937169622@qq.com>
2016-09-22 11:54:49 +0800

    跳转

---
+++ app/src/main/java/com/happy/happyproject/ui/view/TopBar.java
@@ -0,0 +1,66 @@
+package com.happy.happyproject.ui.view;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.happy.happyproject.R;
+import com.happy.happyproject.ui.SpaceFloatBtnActivity;
+
+public class TopBar extends RelativeLayout implements View.OnClickListener {
+    private static final String TAG = TopBar.class.getSimpleName();
+    private ImageView mScan;
+    private TextView mSearch;
+    private TextView mDesigne;
+
+
+    public TopBar(Context context) {
+        this(context,null);
+    }
+
+    public TopBar(Context context, AttributeSet attrs) {
+        this(context, attrs,0);
+    }
+
+    public TopBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context);
+    }
+
+    private void init(Context context) {
+
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.topbar, this,true);
+        mDesigne = ((TextView) view.findViewById(R.id.tobar_image));
+        mScan = ((ImageView) view.findViewById(R.id.topbar_scan));
+        mSearch = ((TextView) view.findViewById(R.id.topbar_search));
+        mScan.setOnClickListener(this);
+        mSearch.setOnClickListener(this);
+        mDesigne.setOnClickListener(this);
+}
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+//            case R.id.topbar_search:
+//                Log.e(TAG, "onClick: search" );
+//                Toast.makeText(getContext(), "此功能暂时没有开放", Toast.LENGTH_SHORT).show();
+//                break;
+            case R.id.topbar_scan:
+                Toast.makeText(getContext(), "此功能暂时没有开放", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tobar_image:
+                Intent intent = new Intent(getContext(), SpaceFloatBtnActivity.class);
+                getContext().startActivity(intent);
+                break;
+        }
+    }
+}
